cd37b485835306ba3c8b3fa89cc81e3b46b94c7f
awaprim <diepl2034@gmail.com>
2026-06-20 17:14:03 +0300

    app id requirement

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use futures_util::StreamExt;
-use std::{env, io::Write, os::unix::net::UnixStream};
+use std::{env, io::Write, os::unix::net::UnixStream, path::PathBuf};
+use tokio::{fs::File, io::AsyncWriteExt};
 
-use ashpd::desktop::global_shortcuts::{GlobalShortcuts, NewShortcut};
+use ashpd::{
+    AppID, WindowIdentifier,
+    desktop::{
+        CreateSessionOptions,
+        global_shortcuts::{BindShortcutsOptions, GlobalShortcuts, NewShortcut},
+    },
+    register_host_app,
+};
 
 unsafe extern "C" {
     fn getuid() -> i32;
@@ -9,6 +17,18 @@ unsafe extern "C" {
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    {
+        // no error handling needed idc about it that much...
+        let home = env!("HOME");
+        let mut path = PathBuf::from(home);
+        path.push(".local");
+        path.push("share");
+        path.push("applications");
+        path.push("com.fisch.Hyprzoom.desktop");
+        let mut file = File::create(path).await.unwrap();
+        file.write_all(include_bytes!("../desktop_file")).await.unwrap();
+    }
+
     let keybind = create_shortcuts().await;
     let mut zoom_rx = keybind.receive_activated().await.unwrap();
     let mut current_zoom = 1.0;
@@ -42,26 +62,23 @@ async fn main() {
             }
         }
         let mut socket = UnixStream::connect(&socket_path).unwrap();
-        socket
-            .write_all(format!("keyword cursor:zoom_factor {current_zoom}").as_bytes())
-            .unwrap();
+        socket.write_all(format!("keyword cursor:zoom_factor {current_zoom}").as_bytes()).unwrap();
     }
 }
 
-async fn create_shortcuts() -> GlobalShortcuts<'static> {
+async fn create_shortcuts() -> GlobalShortcuts {
+    let app_id = AppID::try_from("com.fisch.Hyprzoom").unwrap();
+    register_host_app(app_id).await.unwrap();
+    let options = CreateSessionOptions::default();
     let shortcut = GlobalShortcuts::new().await.unwrap();
-    let session = shortcut.create_session().await.unwrap();
+    let session = shortcut.create_session(options).await.unwrap();
+    let options = BindShortcutsOptions::default();
     let short = NewShortcut::new("zoom", "increases zoom");
-    let bind = shortcut
-        .bind_shortcuts(&session, &[short], None)
-        .await
-        .unwrap();
+    let bind = shortcut.bind_shortcuts(&session, &[short], None, options).await.unwrap();
     bind.response().unwrap();
+    let options = BindShortcutsOptions::default();
     let short = NewShortcut::new("unzoom", "decreases zoom");
-    let bind = shortcut
-        .bind_shortcuts(&session, &[short], None)
-        .await
-        .unwrap();
+    let bind = shortcut.bind_shortcuts(&session, &[short], None, options).await.unwrap();
     bind.response().unwrap();
     shortcut
 }
